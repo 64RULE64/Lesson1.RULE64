e1fc45b8801a48c4efc99adf05a7d7643238dd71
64RULE64 <rule_64@mail.ru>
2024-08-18 23:28:16 +0400

    Add files via upload

---
+++ practice.work1.rule64.py
@@ -0,0 +1,43 @@
+  # "1st program"
+print ('       Start 1st programm')
+print(9 ** 0.5) # 3.0
+print((9 ** 0.5) * 5) # 15.0
+print ('       The 1st programm is ready')
+    # Условие выполнено
+print (' ')
+    # "2nd programm"
+print ('       Start 2nd programm')
+print(9.99 > 9.98 and 1000 != 1000.1) # True
+print ('       The 2nd programm is ready')
+    # Условие выполнено
+print (' ')
+    # "3rd programm"
+print ('       Start 3rd programm')
+print(2*2+2) # 6
+print(2*(2+2)) # 8
+print((2*2+2) == (2*(2+2))) # false
+print ('       The 3rd programm is ready')
+    # Условие выполнено
+print (' ')
+    # "4th programm"
+print ('       Start 4th programm')
+print('123.456')
+    # УСЛОВИЕ : Вывести на экран первую цифру после запятой - 4.
+    # print(float('123.456'))
+(float('123.456')* 10) # перевод c типа данных str на float
+x=(float('123.456')* 10) # присвоения х = значения float
+    # print(x)  получилось -  x = 1234.56
+    # print(type(x)) тип данных - float
+print(x % 10) # деление выделяя остаток print(x % 10) = 4.559999999999945
+y=(x % 10) # присвоение к y = значение деления
+    # print(y)  y = 4.559999999999945
+print(y // 1) # print(y // 1) = 4.0
+    # print(type(y)) тип данных - float
+z=(y // 1) # z = 4.0 деление без остатка
+print((int(z))) # меняем тип данных с дробных float на int - целые значения  - Условие выполнено = 4
+w=(int(z)) # присвоили к w значение = 4
+print(w) # получилось 4
+print ('       The 4th programm is ready')
+# Условие выполнено
+print (' ')
+print('Thank you for the practice, have a nice day =)')
